0d8ef3fa8952a1ca93bb38039d5fc010f56a9f61
Carrie Savinell <carrielane89@gmail.com>
2018-08-07 14:27:02 +0000

    automatically backed up by learn

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-  # def display_artist(artist)
-  #   @artist = Artist.find_by(name: name)
-  # end
+  def display_artist(artist)
+    @artist = Artist.find_by(name: name)
+  end
 end

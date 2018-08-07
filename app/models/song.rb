ba81b2131bb731ad336c468f0602147ce7cee5e5
Carrie Savinell <carrielane89@gmail.com>
2018-08-07 14:21:32 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,10 +4,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    @artist
+    self.artist ? self.artist.name : nil
   end
 
   def artist_name=(name)
-    binding.pry
+    self.artist = Artist.find_or_create_by(name: name)
   end
 end
